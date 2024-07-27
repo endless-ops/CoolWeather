1e1a58524c3bcc65a31cd993095d78374b9c5b08
Iambachelor <15083267362@163.com>
2024-07-27 21:34:19 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/ps/xy/cool/weather/db/Country.java
@@ -0,0 +1,43 @@
+package com.ps.xy.cool.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+
+    private int id ;
+    private String countryName;
+    private int countryCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(int countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
